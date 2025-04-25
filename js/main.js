812d030c4cd0b54840559516d1791bd118be1b21
Tommaso <117763499+Tommy270602@users.noreply.github.com>
2025-04-25 18:42:38 +0200

    add dark mode toggle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Welcome to my portfolio!");
+/* 
+    Button to toggle between light and dark mode
+*/ 
+const button = document.getElementById("themeToggle");
+
+button.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  button.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
+
